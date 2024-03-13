4f9b9f53f94942b23da289f3527bcdeac50de278
Dnyandip Jadhav <jadhavvg77@gmail.com>
2024-03-13 15:53:56 +0530

    Twitter Drawer

---
+++ app/src/main/java/com/example/navigationdrawer/ui/Bookmarks/BookmarksViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.navigationdrawer.ui.Bookmarks
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class BookmarksViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/example/navigationdrawer/ui/Premium/PremiumViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.navigationdrawer.ui.Premium
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PremiumViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
